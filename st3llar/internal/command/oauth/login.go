f2e6d940efa666611d6d24009f2520e7c3f8f482
v3nooonn <v3nooom@outlook.com>
2024-07-24 15:09:23 +0800

    finish 80 percent of the command: login

--- st3llar/internal/command/oauth/login.go
+++ st3llar/internal/command/oauth/login.go
@@ -6,27 +6,28 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	rootCMD "github.com/v3nooom/st3llar/internal/command"
+	"github.com/v3nooom/st3llar/internal/config"
+	"github.com/v3nooom/st3llar/internal/constant"
 	"github.com/v3nooom/st3llar/internal/util"
 	"golang.org/x/term"
+	"gopkg.in/yaml.v3"
 	"os"
 	"syscall"
 )
 
 var (
 	organization string
-	username     string
-	password     string
-	outputPath   string
+	account      string
+	//password     string
 
 	//env          = "dev"
 	//credentialPath = "./credential.json"
 )
 
 type Credential struct {
-	Organization string `json:"organization"`
-	Username     string `json:"username"`
-	Environment  string `json:"env"`
+	Account      string `json:"account"`
 	Token        string `json:"token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 // login represents the sign-in command
@@ -36,25 +37,29 @@ var login = &cobra.Command{
 	Long: `Login to the Stellar auto-action by the pre-established credentials:
 Organization, Account and Password
 
-Available credentials are stored locally in the default path: ./credentials.json, once login successfully.
-And you can use --credentials to specify the path of the credentials file.`,
+Available credential file is stored locally in the default path: $HOME/.st3llar-credentials.
+- using --credential to specify the custom credential file path
+- using --environment to specify the environment stored in the configuration file`,
 	Args:      cobra.OnlyValidArgs,
-	ValidArgs: []string{"organization", "username", "env"},
-	PreRun:    util.PreRunBindFlags,
-	Run:       loginFunc,
+	ValidArgs: []string{"organization", "account", "env"},
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+
+		util.PreRunBindFlags(cmd, args)
+		return nil
+	},
+	Run: loginFunc,
 }
 
 func init() {
 	rootCMD.Root.AddCommand(login)
 
-	login.Flags().StringVarP(&outputPath, "output", "o", "", "the output destination of the current command")
 	login.Flags().StringVarP(&organization, "organization", "O", "", "Name of the organization")
-	login.Flags().StringVarP(&username, "username", "U", "", "Name of the account")
+	login.Flags().StringVarP(&account, "account", "A", "", "Name of the account")
 
 	if err := login.MarkFlagRequired("organization"); err != nil {
 		return
 	}
-	if err := login.MarkFlagRequired("username"); err != nil {
+	if err := login.MarkFlagRequired("account"); err != nil {
 		return
 	}
 }
@@ -69,7 +74,8 @@ func loginFunc(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	password = string(passwordBytes)
+	password := string(passwordBytes)
+	fmt.Printf("input password: %v\n", password)
 
 	fmt.Println()
 	fmt.Println("Login Func:")
@@ -87,24 +93,38 @@ func loginFunc(cmd *cobra.Command, args []string) {
 		fmt.Printf("flag.Name: %v, flag.Value: %v\n", flag.Name, flag.Value)
 	})
 
-	//cred := Credential{
-	//	Organization: viper.GetString(constant.Organization.ValStr()),
-	//	Username:     username,
-	//	Environment:  viper.GetString(constant.Environment.ValStr()),
-	//}
-	//
-	//// Marshal struct to JSON
-	////credJSON, err := json.Marshal(cred)
-	//credJSON, err := json.MarshalIndent(cred, "", "  ")
-	//if err != nil {
-	//	fmt.Fprintln(os.Stderr, "Error marshaling credentials to JSON")
-	//	os.Exit(1)
-	//}
-	//
-	//// Write JSON to file
-	//err = os.WriteFile(credentialPath, credJSON, 0644)
-	//if err != nil {
-	//	fmt.Fprintln(os.Stderr, "Error writing credentials to file")
-	//	os.Exit(1)
-	//}
+	// new credential file
+	cred := Credential{
+		Account:      viper.GetString("account"),
+		Token:        "JWT_TOKEN",
+		RefreshToken: "JWT_REFRESH_TOKEN",
+	}
+
+	credYaml, err := yaml.Marshal(cred)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error marshaling credentials to YAML")
+		os.Exit(1)
+	}
+
+	// Write YAML to file
+	err = os.WriteFile(viper.GetString(constant.FlagCredential.ValStr()), credYaml, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error writing credentials to file")
+		os.Exit(1)
+	}
+
+	// sync to configuration file
+	cfgPath := config.Home() + "/.st3llar"
+	cfg := config.Build(
+		config.WithLogLevel(viper.GetString(constant.FlagLogLevel.ValStr())),
+		config.WithEnvPrefix(viper.GetString(constant.FlagEnvPrefix.ValStr())),
+		config.WithEnvironment(viper.GetString(constant.FlagEnvironment.ValStr())),
+		config.WithOrganization(viper.GetString(constant.FlagOrganization.ValStr())),
+		config.WithCredential(viper.GetString(constant.FlagCredential.ValStr())),
+	)
+
+	if err := config.WriteConfig(cfg, cfgPath); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
 }
